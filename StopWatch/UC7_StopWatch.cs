8dcd07a08f51908556c423df6b04f9d6e5f977ab
Arunsh93 <Arunhosur93@gmail.com>
2021-09-24 15:50:19 +0530

    Stop watch Problem

---
+++ StopWatch/UC7_StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class UC7_StopWatch
+    {
+        static void Main(string[] args)
+        {
+            long startTimer;
+            long stopTimer;
+            Console.WriteLine("Press '1' to start time: ");
+            startTimer = long.Parse(Console.ReadLine());
+            Console.WriteLine("Start time is: " + startTimer);
+            startTimer = startTimer * 1000;
+
+            Console.WriteLine("Press '2' to stop time: ");
+            stopTimer = long.Parse(Console.ReadLine());
+            Console.WriteLine("Start time is: " + stopTimer);
+            stopTimer = stopTimer * 1000;
+            long elapsed = stopTimer - startTimer;
+            Console.WriteLine("Total elapsed time in mileStones is: " + elapsed);
+        }
+    }
+}
