b1528a6309581b2eadd6576b7d6e8d5e0312661c
Arunsh93 <Arunhosur93@gmail.com>
2021-09-24 13:29:26 +0530

    Fibonacci Series Program

---
+++ FibonacciSeries/UC1_FibonassiSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class UC1_FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            int Number1 = 0;
+            int Number2 = 1;
+            int Number3;
+            int Number;
+
+            Console.Write("Enter the Number to Fibonacci Series: ");
+            Number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Number1 + " " + Number2 + " ");
+
+            for(int i = 2; i<Number; i++)
+            {
+                Number3 = Number1 + Number2;
+                Console.WriteLine(Number3 + " ");
+                Number1 = Number2;
+                Number2 = Number3;
+            }
+        }
+    }
+}
